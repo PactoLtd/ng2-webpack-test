1fd5de748495e72839c4bb02e4340b5bbe358669
Roman Tiskiy <roman@pacto.io>
2017-02-13 12:16:01 +0200

    Creating AngularJS project

---
+++ app.js
@@ -0,0 +1,2 @@
+const child1 = require('./child1.js');
+const child2 = require('./child2.js');
---
+++ dist/app.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/romantiskiy/workspace/ng2-webpack-test/src/bootstrap\""); }());
+
+
+/***/ }
+]);
+//# sourceMappingURL=app.bundle.js.map
